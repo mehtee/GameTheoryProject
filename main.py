f8302e24858eb9a17baa89fc89fd19797cd4c753
Mahdi Rasouli <mahdirasouli007@gmail.com>
2022-06-19 00:20:51 +0430

    🔨 Implemented MaxMin Strategy and Value
    🔨 Implemented MaxMin Strategy and Value

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cmath import inf
 import numpy as np
 
 n = int(input())
@@ -12,8 +13,34 @@ for i in range(n):
     Bi = list(map(int, input().split()))
     B.append(Bi)
 
-A, B = np.array(A), np.array(B)
-print(A,B)
-
 assert len(A) == len(B) and len(A[0]) == len(B[0])
 
+def maxmin(payoff, i):
+    # Minimum of each row of payoff matrix (i.e. each strategy of player i)
+    minimums_of_player = []
+
+    # for each strategy of player i
+    for iStrategy in range(n):
+        # getting row of ith strategy of player i 
+        # (all payoffs corresponding to the strategy i of player i)
+        Ai = payoff[iStrategy]
+        # minimum of those payoffs
+        minAi = min(Ai)
+        # adding to the min array
+        minimums_of_player.append(minAi)
+
+    # maximum of minimums
+    max_val_player = max(minimums_of_player)
+    # saving indices (1-based) of maxmin (i.e. the ith maxmin strategies)
+    max_indices = []
+    # search which elements' indices are those of that maxmin value:
+    for index, element in enumerate(minimums_of_player):
+        if element == max_val_player:
+            # and appending each of them to the max_indices array
+            max_indices.append(str(index+1))
+
+    print("Maxmin values and strategy player", i)
+    print(max_val_player, "-", ",".join(max_indices), sep="")
+
+maxmin(A, 1)
+maxmin(B, 2)
